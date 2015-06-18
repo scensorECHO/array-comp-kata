62988e5446f134bf2114b78c5da6a8e6f6c6b6a0
scensorECHO <xxartofwarxx@gmail.com>
2015-06-18 18:59:08 -0400

    Create AreSame.java

---
+++ AreSame.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class AreSame {
+	
+	public static boolean comp(int[] a, int[] b) {
+    if(a.length != b.length)
+      return false;
+    LinkedList<Integer> listB = new LinkedList<>(Arrays.asList(b));
+    for(int i=0;i<a.length;i++){
+      ListIterator<Integer> iter = listB.listIterator();
+      while(iter.hasNext()){
+        if(iter.next()==a[i]){
+          iter.remove();
+          break;
+        }
+        return false;   
+      }
+    }
+    return true;
+    
+  }
+}
